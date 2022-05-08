353654ae95cf1a128ea5e1712c1874129711d933
Liem Vu <50932746+MrLiemVu@users.noreply.github.com>
2022-05-07 19:49:20 -0700

    Create handObject.py

---
+++ handObject.py
@@ -0,0 +1,12 @@
+class hand:
+  def __init__(self, position=[0,0,0], velocity=[0,0,0], acceleration=[0,0,0]):
+    self.position = position
+    self.velocity = velocity
+    self.acceleration = acceleration
+  
+# This more important if we do piano or guitar, maybe just do later
+# class finger:
+#   def __init__(self, position=[0,0,0], velocity=[0,0,0], acceleration=[0,0,0]):
+#     self.position = position
+#     self.velocity = velocity
+#     self.acceleration = acceleration
