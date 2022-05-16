d2e878881a271156ac3bcb21a8ad4606493270fe
Liem Vu <50932746+MrLiemVu@users.noreply.github.com>
2022-05-16 13:53:17 -0700

    Update drumObjects.py

--- drumObjects.py
+++ drumObjects.py
@@ -9,33 +9,6 @@ class drumset:
     self.z = z
     self.radius = radius
     
-  def play_sound(soundFile):
-    #define stream chunk   
-    chunk = 1024  
-
-    #open a wav format music  
-    f = wave.open(soundFile,"rb")  
-    #instantiate PyAudio  
-    p = pyaudio.PyAudio()  
-    #open stream  
-    stream = p.open(format = p.get_format_from_width(f.getsampwidth()),  
-                    channels = f.getnchannels(),  
-                    rate = f.getframerate(),  
-                    output = True)  
-    #read data  
-    data = f.readframes(chunk)  
-
-    #play stream  
-    while data:  
-        stream.write(data)  
-        data = f.readframes(chunk)  
-
-    #stop stream  
-    stream.stop_stream()  
-    stream.close()  
-
-    #close PyAudio  
-    p.terminate()  
     
 class snare(drumset):
   def __init__(self, sound, x=0, y=0, z=0, radius=5):
